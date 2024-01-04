387b20990e7f352c28a40009d2c41c58c7281ed4
Saba Shakeraneh <saba_sana1395@yahoo.com>
2024-01-05 03:06:37 +0330

    Add BMI calculation code to js

--- BMI-10.js
+++ BMI-10.js
@@ -1,2 +1,21 @@
-const weight = prompt("what is your weight in kg?");
-const height = prompt("what is your height in meter?");
+const weight = prompt("What is your weight in kg?");
+const height = prompt("What is your height in meter?");
+
+  if (weight && height) {
+      bmi = Number(weight) / (Number(height) ** 2);
+      if (bmi < 18.5) {
+        alert("Under Weight");
+      }else if (bmi >= 18.5 && bmi < 25) {
+        alert("Normal Weight");
+      }else if (bmi >= 25 && bmi < 30) {
+        alert("Over Weight");
+      }else if (bmi >= 30 && bmi < 35) {
+        alert("Obesity (Class 1)");
+      }else if (bmi >= 35 && bmi < 40) {
+        alert("Obesity (Class 2)");
+      }else{
+        alert("Extreme Obesity");
+      }
+    }else{
+      alert("Please enter your weight and height");
+    }
---
+++ README.md
@@ -0,0 +1,4 @@
+# BMI-calculator
+
+
+This is a measure of body fat based on height and weight that applies to adult men and women.
